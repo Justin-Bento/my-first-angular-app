018ab95c571a938348fb9f38f9011739b4340f08
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2026-09-18 14:53:21 -0400

    feat(services): added route to  services component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Home } from './home/home';
+import { Services } from './services/services';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: "about",
     component: About
+  },
+  {
+    path: "services",
+    component: Services
   }
 ];
